e9373d9be0029a40507c4ae1c3e4f5ab14675568
repictjasa <65887264+repictjasa@users.noreply.github.com>
2023-04-02 12:57:21 +0200

    inserted templates for needed functions

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import cv2
 
 
+def my_roberts(slika):
+    # vaša implementacija
+    return None
+
+
+def my_prewitt(slika):
+    # vaša implementacija
+    return None
+
+
+def my_sobel(slika):
+    # vaša implementacija
+    return None
+
+
+def canny(slika, sp_prag, zg_prag):
+    # vaša implementacija
+    return None
+
+
+def spremeni_kontrast(slika, alfa, beta):
+    pass
+
+
 def showImage(name, img):
     cv2.imshow(name, img)
 
