ace373c96f809dad44b4825d2bafb755118cebcf
repictjasa <65887264+repictjasa@users.noreply.github.com>
2023-04-02 15:44:09 +0200

    Added function for applying 2x2 filters on image. Added Roberts kernels.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 # Nad originalno sliko uporabimo vertikalni in horizontalni kernel
-def apply_filter(img, x_kernel, y_kernel):
+def apply_filter_3x3(img, x_kernel, y_kernel):
     img_output = np.zeros(img.shape, np.uint8)
     x_filter = np.zeros(img.shape, np.uint8)
     y_filter = np.zeros(img.shape, np.uint8)
@@ -33,10 +33,41 @@ def apply_filter(img, x_kernel, y_kernel):
 
     return img_output
 
+# Enako kot zgornja funkcija le da deluje nad 2x2 kernelom
+def apply_filter_2x2(img, x_kernel, y_kernel):
+    img_output = np.zeros(img.shape, np.uint8)
+    x_filter = np.zeros(img.shape, np.uint8)
+    y_filter = np.zeros(img.shape, np.uint8)
 
-def my_roberts(slika):
-    # vaša implementacija
-    return None
+    for y in range(1, img.shape[0] - 1):
+        for x in range(1, img.shape[1] - 1):
+            x_filter[y][x] = np.absolute((
+                    x_kernel[0][1] * img[y - 1][x] +
+                    x_kernel[1][1] * img[y][x] +
+                    x_kernel[1][0] * img[y][x - 1] +
+                    x_kernel[0][0] * img[y - 1][x - 1]
+            ))
+            y_filter[y][x] = np.absolute((
+                    y_kernel[0][0] * img[y - 1][x - 1] +
+                    y_kernel[0][1] * img[y - 1][x] +
+                    y_kernel[1][1] * img[y][x] +
+                    y_kernel[1][0] * img[y][x - 1]
+            ))
+
+            img_output[y][x] = sqrt(pow(y_filter[y][x], 2) + pow(x_filter[y][x], 2))
+
+    return img_output
+
+
+def my_roberts(img):
+    roberts_x_kernel = np.array([[1, 0],
+                                 [0, -1]])
+
+    roberts_y_kernel = np.array([[0, 1],
+                                 [-1, 0]])
+
+    return apply_filter_2x2(img, roberts_x_kernel, roberts_y_kernel)
+
 
 def my_prewitt(img):
     prewitt_x_kernel = np.array([[1, 0, -1],
@@ -47,7 +78,7 @@ def my_prewitt(img):
                                  [0, 0, 0],
                                  [-1, -1, -1]])
 
-    return apply_filter(img, prewitt_x_kernel, prewitt_y_kernel)
+    return apply_filter_3x3(img, prewitt_x_kernel, prewitt_y_kernel)
 
 
 def my_sobel(img):
@@ -59,7 +90,7 @@ def my_sobel(img):
                                [0, 0, 0],
                                [-1, -2, -1]])
 
-    return apply_filter(img, sobel_x_kernel, sobel_y_kernel)
+    return apply_filter_3x3(img, sobel_x_kernel, sobel_y_kernel)
 
 
 def canny(slika, sp_prag, zg_prag):
@@ -80,6 +111,8 @@ def main():
     original = img
 
     showImage("Slika", original)
+
+    showImage("Slika Roberts", my_roberts(img))
 
     showImage("Slika Sobel", my_sobel(img))
 
